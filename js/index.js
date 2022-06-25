2280c763f2f8bf335481b1bdfc9908477a4bcb4d
Aleks Haran <nporock@gmail.com>
2022-06-25 17:27:42 +0300

    add-css

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const clients = ["Mango", "Ajax", "Poly"];
+
+for (const cli of clients) {
+  console.log(cli);
+}
+
+const string = "javascript";
+
+for (const char of string) {
+  console.log(char);
+}
